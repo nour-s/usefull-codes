1b18249e505b3427349621928f0b499ed39fbf60
Nour Sabouny <programmer.nour@gmail.com>
2017-01-12 10:21:58 +0400

    Update add-jquery-to-any-page.js

--- add-jquery-to-any-page.js
+++ add-jquery-to-any-page.js
@@ -1,6 +1,6 @@
 javascript: (function () {
     var s = document.createElement('script');
-    s.setAttribute('src', 'http://code.jquery.com/jquery-latest.min.js');
+    s.setAttribute('src', 'https://code.jquery.com/jquery-latest.min.js');
     if (typeof jQuery == 'undefined') { document.getElementsByTagName('head')[0].appendChild(s);  }
     jQuery("td.edit select option[value=BN]").attr("selected", "");
     })();
