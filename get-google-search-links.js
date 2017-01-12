159ac54f105b60cf2d295005b67ca98804b78426
Nour Sabouny <programmer.nour@gmail.com>
2017-01-12 12:14:06 +0400

    Update get-google-search-links.js

--- get-google-search-links.js
+++ get-google-search-links.js
@@ -3,3 +3,12 @@ $('.srg > .g > .rc > .r > a').each(function (i, x){ console.log(x.href);}); // y
 
 //if you are sure that they are downloadable files, you can use this to download all of them at once.
 $('.srg > .g > .rc > .r > a').each(function (i, x){ window.open(x.href, "_blank");  })
+
+//to read links from a web page and then download them
+$.get('https://raw.githubusercontent.com/ZhaoLion/O-Reilly-Offering-Programming-eBooks-for-Free/master/O-Reilly-Offering-Programming-eBooks-for-Free.txt',
+null, 
+function(data){
+	var lines = data.split('\n');
+	for(var l = 0; l< lines.length; l++)
+		window.open(lines[l], "_blank");
+}, null)
