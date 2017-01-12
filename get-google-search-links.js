dcb15445d4f7348fef64d04e65cbcc32bbee5030
Nour Sabouny <programmer.nour@gmail.com>
2017-01-12 11:14:11 +0400

    Update get-google-search-links.js

--- get-google-search-links.js
+++ get-google-search-links.js
@@ -1,1 +1,5 @@
-$('.srg>.g>.rc>.r>a').each(function (i, x){ console.log(x.href);}); // you need Jquery for this.
+//This will give you all the links in result page of google search.
+$('.srg > .g > .rc > .r > a').each(function (i, x){ console.log(x.href);}); // you need Jquery for this.
+
+//if you are sure that they are downloadable files, you can use this to download all of them at once.
+$('.srg > .g > .rc > .r > a').each(function (i, x){ window.open(x.href, "_blank");  })
