e0a207e15fbed4fc218f3d38334274ad0d9de586
Nour Sabouny <programmer.nour@gmail.com>
2017-10-22 11:25:07 +0400

    Create outline-all-elements.js

---
+++ outline-all-elements.js
@@ -0,0 +1,9 @@
+javascript: (function () {
+    if(typeof $$ == 'undefined')
+    {
+    var s = document.createElement('script');
+    s.setAttribute('src', 'https://code.jquery.com/jquery-latest.min.js');
+    if (typeof jQuery == 'undefined') { document.getElementsByTagName('head')[0].appendChild(s);  }
+    }
+    [].forEach.call($$("*"),function(a){a.style.outline="1px solid #"+(~~(Math.random()*(1<<24))).toString(16)})
+    })();
