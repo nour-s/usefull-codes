887d3fb772f48e353e2857266e78640c18045117
Nour Sabouny <programmer.nour@gmail.com>
2017-10-22 12:03:00 +0400

    Update outline-all-elements.js

--- outline-all-elements.js
+++ outline-all-elements.js
@@ -1,9 +1,9 @@
-function outline () {
-    if(typeof $$ == 'undefined')
+(function outline () {
+    if(typeof jQuery == 'undefined')
     {
     var s = document.createElement('script');
     s.setAttribute('src', 'https://code.jquery.com/jquery-latest.min.js');
     if (typeof jQuery == 'undefined') { document.getElementsByTagName('head')[0].appendChild(s);  }
     }
-    [].forEach.call($$("*"),function(a){a.style.outline="1px solid #"+(~~(Math.random()*(1<<24))).toString(16)})
-    }
+    [].forEach.call($("*"),function(a){a.style.outline="1px solid #"+(~~(Math.random()*(1<<24))).toString(16)})
+    })();
